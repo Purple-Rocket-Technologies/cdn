6920965a1389f3a5c5bada9e2565d262f564d2b3
michiamodavide <ashishavik11@icloud.com>
2021-08-10 19:25:17 +0530

    Create appointment.js

---
+++ discoverfin/appointment.js
@@ -0,0 +1,20 @@
+var user_name = readCookie('Name');
+$('.user_name').each(function(){
+    $(this).html(''+user_name);
+});
+
+var appointment_link = readCookie('APTMT_LINK');
+$('#aptmt_link1').attr('href',appointment_link);
+$('#aptmt_link2').attr('href',appointment_link);
+$('#aptmt_link3').attr('href',appointment_link);
+
+var rep_name = capitalize(readCookie('REP_NAME'));
+$('.rep_name').text(rep_name);
+
+var rep_phone = readCookie('PHONE');
+$('#phone').text(rep_phone);
+
+var rep_email = readCookie('EMAIL');
+$('#email_id').text(rep_email);
+
+$('.apt-rep-image').css("background-image","url('"+ readCookie('PIC')+"')");
