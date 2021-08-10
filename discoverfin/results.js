36278265595355b92c0e31cd2a449824a994ffdf
michiamodavide <ashishavik11@icloud.com>
2021-08-10 19:25:49 +0530

    Create results.js

---
+++ discoverfin/results.js
@@ -0,0 +1,10 @@
+import "https://teamepyc.github.io/cdn/discoverfin/js/scratch.js";
+
+//reading cookies
+var fin_num = addCommas(parseInt(readCookie("FIN Number")));
+$("#fin_number").html("$" + fin_num);
+var user_name = readCookie("Name");
+
+$(".user_name").each(function () {
+  $(this).html("" + user_name);
+});
