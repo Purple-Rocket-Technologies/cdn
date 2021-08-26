4ce582f7d2c3f9f8aae4b01fac7e962e4890ca7a
michiamodavide <ashishavik11@icloud.com>
2021-08-26 16:56:48 +0530

    Update appointment.js

--- discoverfin/appointment.js
+++ discoverfin/appointment.js
@@ -1,40 +1,89 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+}
+
+
 var user_name = readCookie('Name');
-$('.user_name').each(function(){
-    $(this).html(''+user_name);
+var appointment_link;
+var rep_name;
+var rep_phone;
+var rep_email;
+var rep_pic;
+var video_id;
+var user = getUrlParameter('user');
+var company = getUrlParameter('company');
+
+atomic('https://' + api_url + '/api/v1/users/getCompany/name/' + company + '/' + user, {
+    method: 'GET',
+
+})
+.then(function (response) {
+    if (response.data.error == true) {
+        console.log('Error');            
+    }
+    else
+    {       
+        console.log(response.data);          
+        appointment_link = response.data.data.appointmentBookingLink;
+        rep_name = response.data.data.firstName;
+        rep_pic = response.data.data.profilePic;
+        rep_phone = response.data.data.phone;
+        rep_email = response.data.data.email;
+        video_id = $.trim(response.data.data.videoProfileLink);
+        map_all_data();
+    }
+})
+.catch(function (error) {
+    console.log(error.status); // xhr.status
+    console.log(error.statusText); // xhr.statusText
 });
 
-var appointment_link = readCookie('APTMT_LINK');
-$('#aptmt_link1').attr('href',appointment_link);
-$('#aptmt_link2').attr('href',appointment_link);
-$('#aptmt_link3').attr('href',appointment_link);
 
-var rep_name = capitalize(readCookie('REP_NAME'));
-$('.rep_name').text(rep_name);
+function map_all_data(){
+    if(user_name){
+        $('.user_name').each(function(){
+            $(this).html(''+user_name);
+        });
+    } else {
+        $('.user_name').addClass('hide');
+    }    
+    
+    $('#aptmt_link1').attr('href',appointment_link);
+    $('#aptmt_link2').attr('href',appointment_link);
+    $('#aptmt_link3').attr('href',appointment_link);
+    
+    $('.rep_name').text(rep_name);
+    
+    $('#phone').text(rep_phone);
+    
+    $('#email_id').text(rep_email);
+    
+    $('.apt-rep-image').css("background-image","url('"+ rep_pic +"')");    
 
-var rep_phone = readCookie('PHONE');
-$('#phone').text(rep_phone);
-
-var rep_email = readCookie('EMAIL');
-$('#email_id').text(rep_email);
-
-$('.apt-rep-image').css("background-image","url('"+ readCookie('PIC')+"')");
+    if(video_id){
+        $('.apt-hero-bottomsection').addClass('active');
+        $('.apt-reps-video').append(video_id);
+        /*var video_container_width = (($('.apt-reps-video').width())/16)*9;
+        $('.apt-reps-video iframe').attr('height','100%');
+        $('.apt-reps-video iframe').attr('width','100%');        */
+    }    
+}
+       
 
 $('.closer-last').click(function(){
     $('#window_frame').attr("src","/appointment");
 });
-
-
-var video_id = $.trim(readCookie('VIDEO'));
-const aptmturl = new URL(video_id);
-videotokenID = aptmturl.pathname
-videotokenID = videotokenID.replace('/','');
-iframeUrl = "https://player.vimeo.com/video/" + videotokenID + "?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;h=ed9a32a11a";
-
-
-if(video_id != ""){
-    $('#aptmt_video').attr('src',iframeUrl);
-    $('.apt-hero-bottomsection').addClass('active');    
-}
 
 
 
