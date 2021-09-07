ad5392fd79916a5e4025b11fbcb78451f2b11cf0
michiamodavide <ashishavik11@icloud.com>
2021-09-08 03:57:24 +0530

    Update questions.js

--- discoverfin/development/questions.js
+++ discoverfin/development/questions.js
@@ -1,8 +1,27 @@
-function isEmail(e) {
-  return /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(
-    e
-  );
+var relativ_url = window.location.origin + window.location.pathname;
+var curren_url = window.location;
+if (curren_url != relativ_url) {
+  window.location = relativ_url;
 }
+
+$("body").on("scroll mousewheel touchmove", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  return false;
+});
+
+
+function initializeVar() {
+  avg_retirement_age = 0;
+  default_death_age = 0;
+}
+
+function isEmail(e) {
+  return /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(e);
+}
+
+initializeVar();
+
 
 $(".button_blocker.email").click(function () {
   alert("Please enter a correct email");
@@ -19,31 +38,6 @@ $(".text-field.email").keyup(function () {
       $(".button_blocker.email").removeClass("hide");  
     }  
 });
-
-
-var unique_url = "mayank";
-var user_url = "mayank";
-var user_id = "1n6kcukwtrg";
-$("#unique_id").val("1n6kcukwtrg");
-if (
-  readCookie("URL") != "" &&
-  readCookie("ID") != "" &&
-  readCookie("USER_URL") != "" &&
-  readCookie("USER_ID") != ""
-) {
-  unique_url = readCookie("URL");
-  user_url = readCookie("USER_URL");
-  user_id = readCookie("USER_ID");
-  $("#unique_id").val("" + readCookie("ID"));
-}
-initializeVar();
-function initializeVar() {
-  avg_retirement_age = 0;
-  default_death_age = 0;
-}
-
-
-
 
 $("#guessed_fin").val("skipped");
 
@@ -71,7 +65,7 @@ $("#user_name").on("keypress", function (e) {
   }
 });
 
-$("#age_dummy").keyup(function () {
+$("#user_age").keyup(function () {
   var var_age = $(this).val();
   if (var_age.length > 1) {
     if (var_age > 17 && var_age < 71) {
@@ -88,7 +82,8 @@ $("#age_dummy").keyup(function () {
   }
 });
 
-$("#age_dummy").on("keypress", function (e) {
+//question two desktop 1
+$("#user_age").on("keypress", function (e) {
   if (e.which == 13) {
     e.preventDefault();
     if ($("#ques_2_btn").hasClass("go_ahead")) {
@@ -96,7 +91,7 @@ $("#age_dummy").on("keypress", function (e) {
     }
   }
 });
-
+//avg retirement age
 $(".options_popup.step_3 .options_container .option").click(function () {
   valueee = $(this).attr("data-age");
   valueee2 = $(this).attr("data-default");
@@ -105,28 +100,9 @@ $(".options_popup.step_3 .options_container .option").click(function () {
   default_death_age = parseInt(valueee2);
 });
 
-$("#dummy_income").keyup(function () {
-  var dummy_income = $(this).val();
-  $("#user_income").val(dummy_income);
-
-  if ($("#user_income").val() != "") {
-    if (parseInt($("#user_income").val()) > 0) {
-      var income = parseInt(
-        $("#user_income").val().replace(/[^\d]/g, "").replace(/,/g, ""),
-        10
-      ).toLocaleString();
-      $("#user_income").val(income);
-    } else {
-      $("#user_income").val("");
-    }
-  } else {
-    $("#user_income").val("");
-  }
-
-  var var_income = $("#user_income")
-    .val()
-    .replace(/,/g, "")
-    .replace(/[^\d]/g, "");
+//Question 4 Validation
+$("#user_income").keyup(function () {
+  var var_income = $(this).val().replace(/,/g, "").replace(/[^\d]/g, "");
   if (var_income > 999) {
     $("#ques_four_active").addClass("active");
     $("#ques_4_btn").addClass("go_ahead");
@@ -141,6 +117,24 @@ $("#dummy_income").keyup(function () {
   }
 });
 
+// Live Comma
+$("#user_income").keyup(function () {
+  if ($(this).val() != "") {
+    if (parseInt($(this).val()) > 0) {
+      var income = parseInt(
+        $(this).val().replace(/[^\d]/g, "").replace(/,/g, ""),
+        10
+      ).toLocaleString();
+      $(this).val(income);
+    } else {
+      $(this).val("");
+    }
+  } else {
+    $(this).val("");
+  }
+});
+
+//question four desktop 1
 $("#user_income").on("keypress", function (e) {
   if (e.which == 13) {
     e.preventDefault();
@@ -149,6 +143,8 @@ $("#user_income").on("keypress", function (e) {
     }
   }
 });
+
+//inflation calculation ques_4_btn
 
 var year_left_in_retirement;
 var fin_factor;
@@ -169,7 +165,6 @@ $("#ques_4_btn").click(function () {
   $("#income_after_inflation").val(parseInt(income_after_inflation));
   $("#at_age").html(avg_retirement_age);
 });
-
 $(".options_popup.step_3 .option").click(function () {
   $(".options_popup.step_3 .option").removeClass("active");
   $(this).addClass("active");
@@ -181,7 +176,6 @@ $(".options_popup.step_3 .option").click(function () {
   $("#ques_3_active").addClass("active");
   $("#ques_3_btn").addClass("go_ahead");
 });
-
 $(".options_popup.step_5 .option").click(function () {
   $(".options_popup.step_5 .option").removeClass("active");
   $(this).addClass("active");
@@ -192,7 +186,6 @@ $(".options_popup.step_5 .option").click(function () {
   $("#ques_5_active").addClass("active");
   $("#ques_5_btn").addClass("go_ahead");
   $("#pension_choice").val(ans1);
-
   var fin_array = [
     4.7, 5.5, 6.2, 6.9, 7.7, 8.5, 9.18, 9.86, 10.54, 11.22, 11.9, 12.48, 13.06,
     13.64, 14.22, 14.8, 15.3, 15.8, 16.3, 16.8, 17.3, 17.74, 18.18, 18.62,
@@ -201,12 +194,10 @@ $(".options_popup.step_5 .option").click(function () {
   ];
   var ylir = default_death_age;
   fin_factor = fin_array[ylir - 5];
-  //calcuating_fin_number
   var fin_num =
     $(".income_after_inflation").html().replace(/,/g, "") * fin_factor;
   selected_ans = parseInt($(this).attr("data-ans"));
   user_age = parseInt($("#user_age").val());
-
   if (
     (user_age < 30 && selected_ans == 1) ||
     (user_age < 30 && selected_ans == 3)
@@ -227,10 +218,8 @@ $(".options_popup.step_5 .option").click(function () {
   } else {
     fin_num = fin_num;
   }
-
   $("#fin_number").val(fin_num);
 });
-
 $(".guess_option").click(function () {
   $(".guess_option").removeClass("active");
   $(this).addClass("active");
@@ -249,80 +238,91 @@ $("#guess_back").click(function () {
   $("#guessed_fin").val("skipped");
 });
 
-var country = readCookie('country');
-
-$("#submit_btn").click(function () {
-  unique_id = $("#unique_id").val();
-  user_id = user_id;
-  name = $("#user_name").val();
-  age = $("#user_age").val();
-  retirement_age = $("#retirement_age").val();
-  income_after_inflation = parseInt($("#income_after_inflation").val());
-  income_befor_inflation = parseInt($("#user_income").val().replace(/,/g, ""));
-  pension_choice = $("#pension_choice").val();
-  guessed_fin = $("#guessed_fin").val();
-  email = $("#email").val();
-  fin_number = parseInt($("#fin_number").val());
-  country_name = country;
-
-  atomic("https://" + api_url + "/api/v1/users/submit/form", {
-    method: "POST",
-    data: {
-      "Unique-Id": unique_id,
-      "User-Id": user_id,
-      "Name": name,
-      "Age": age,
-      "Retirement-Age": retirement_age,
-      "Annual-Income-after-inflation": income_after_inflation,
-      "Annual-Income-before-Inflation": income_befor_inflation,
-      "Pension-Choice": pension_choice,
-      "Guessed-FIN": guessed_fin,
-      "Email": email,
-      "Fin-Number": fin_number,
-      "country": country_name,
-    },
-  })
-    .then(function (response) {
-      console.log(response.data);
-      window.location.href = "/result";
-      console.log(response.xhr);
-    })
-    .catch(function (error) {
-      console.log(error.status);
-      console.log(error.statusText);
-    });
-
-  function setCookies(field1, value1) {
-    document.cookie = field1 + "=" + value1 + ";path=/";
+var country_val = "United States";
+$(".radio-button-field").click(function () {
+  if ($(this).children("input").prop("checked") == true) {
+    country_val = $(this).children("input").attr("value");
   }
-  setCookies("Unique ID", "" + unique_id);
-  setCookies("Unique URL", unique_url);
-  setCookies("User ID", "" + user_id);
-  setCookies("User URL", user_url);
-  setCookies("Name", "" + name);
-  setCookies("Age", "" + age);
-  setCookies("Retirement Age", "" + retirement_age);
-  setCookies("Income After Inflation", "" + income_after_inflation);
-  setCookies("Income Before Inflation", "" + income_befor_inflation);
-  setCookies("Pension Choice", "" + pension_choice);
-  setCookies("Guessed FIN", "" + guessed_fin);
-  setCookies("Email", "" + email);
-  setCookies("FIN Number", "" + fin_number);
-  setCookies("Country", "" + country_name);
 });
 
-$("*").animate({ scrollTop: 0 }, "fast");
 
-$(".step_btn").click(function () {
-  $("*").scrollTop(0);
+function createNewProspect(){
+  axios({
+    method: 'post',
+    url: 'https://' + api_url + '/api/v1/users/company/'+ readCookie('COMPANY_ID') +'/prospects',
+    data: {
+      companyId: readCookie('COMPANY_ID'),
+      userId: readCookie('USER_ID'),
+      first_name: $("#user_name").val(),
+      age: $("#user_age").val(),
+      retirement_age: $("#retirement_age").val(),
+      annual_income_after_inflation: parseInt($("#income_after_inflation").val()),
+      annual_income_before_inflation: parseInt($("#user_income").val().replace(/,/g, "")),
+      pension_choice: $("#pension_choice").val(),
+      guessed_fin: $("#guessed_fin").val(),
+      email: $("#email").val(),
+      fin_number: parseInt($("#fin_number").val()),
+      country: country_val,        
+    }   
+  })
+  .then(function(response) {  
+    setCookies("prospect_id",response.data.data._id);
+    setCookies("FIN Number", "" + response.data.data.fin_number);
+    setCookies("Name",response.data.data.first_name);
+    setCookies("Country", response.data.data.country);
+    window.location.href = "/result";
+  })
+  .catch(function (error) {    
+    alert("Oops, There was an unexpected error."); 
+  });  
+}
+
+function updateProspect(prospectID){
+  axios({
+    method: 'put',
+    url: 'https://' + api_url + '/api/v1/users/company/'+ readCookie('COMPANY_ID') +'/prospects/' + prospectID,    
+    data: {
+      age: $("#user_age").val(),
+      retirement_age: $("#retirement_age").val(),
+      annual_income_after_inflation: parseInt($("#income_after_inflation").val()),
+      annual_income_before_inflation: parseInt($("#user_income").val().replace(/,/g, "")),
+      pension_choice: $("#pension_choice").val(),
+      guessed_fin: $("#guessed_fin").val(),
+      fin_number: parseInt($("#fin_number").val()),       
+    }   
+  })
+  .then(function(response) {     
+    setCookies("prospect_id",response.data.data._id);
+    setCookies("FIN Number", "" + response.data.data.fin_number);
+    setCookies("Name",response.data.data.first_name);
+    setCookies("Country", response.data.data.country);
+    window.location.href = "/result";
+  })
+  .catch(function (error) {
+    alert("Oops, There was an unexpected error."); 
+  });  
+}
+
+
+$("#submit_btn").click(function () {   
+  axios({
+    method: 'get',
+    url: 'https://' + api_url + '/api/v1/users/company/'+ readCookie('COMPANY_ID') +'/prospects?email='+$("#email").val(),
+  })
+  .then(function(response) {    
+    if(response.data.count == 0){
+      createNewProspect();
+    } else {
+      updateProspect(response.data.data[0]._id);
+    }
+  })
+  .catch(function (error) {
+    alert("Oops, There was an unexpected error."); 
+  });
 });
-$("input").blur(function () {
-  $("*").scrollTop(0);
-});
-$("input").focusout(function () {
-  $("*").scrollTop(0);
-});
-$("#age_dummy").keyup(function () {
-  var dummy_val = $(this).val();
-  $("#user_age").val(dummy_val);
-});
+
+
+
+
+
+  
