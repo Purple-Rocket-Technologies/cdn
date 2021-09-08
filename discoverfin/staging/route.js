7e618d081b6b0b4ef02b50da9b1c37a44a94301d
michiamodavide <ashishavik11@icloud.com>
2021-09-08 19:51:20 +0530

    Update route.js

--- discoverfin/staging/route.js
+++ discoverfin/staging/route.js
@@ -1,10 +1,6 @@
-
-
 var company_id, prospect_id, route_selection, user_name, fin_num;
 
-if(getUrlParameter('company') && getUrlParameter('user') && getUrlParameter('prospectEmail')){
-    getAPIparams();    
-} else if(readCookie('COMPANY_ID') && readCookie('prospect_id') && readCookie('Name') && readCookie('FIN Number')){
+if(readCookie('COMPANY_ID') && readCookie('prospect_id') && readCookie('Name') && readCookie('FIN Number')){
     company_id = readCookie('COMPANY_ID');     
     prospect_id = readCookie('prospect_id');
     user_name = readCookie('Name');
@@ -14,47 +10,6 @@ if(getUrlParameter('company') && getUrlParameter('user') && getUrlParameter('pro
     window.location.href = '/404';
 }
 
-//fetching company, user and prospect IDs
-function getAPIparams() {
-    axios({
-        method: 'get',
-        url: 'https://' + api_url + '/api/v1/users/getCompany/name/' + getUrlParameter('company') + '/' + getUrlParameter('user'),
-      })
-      .then(function(response) {    
-        company_id = response.data.data.companyId;       
-        setCookies("COMPANY_ID",company_id);
-
-        // getting prospect id
-        axios({
-            method: 'get',
-            url: 'https://' + api_url + '/api/v1/users/company/'+ company_id +'/prospects?email=' + getUrlParameter('prospectEmail'),
-
-        }).then(function(response) {  
-
-            if(response.data.count > 0){
-                prospect_id = response.data.data[0]._id;
-                user_name = response.data.data[0].first_name;
-                fin_num = response.data.data[0].fin_number;
-
-                setCookies("prospect_id",prospect_id);
-                setCookies("Name",user_name);
-                setCookies("FIN Number",fin_num);
-                setPageVariableValue();
-            } else {
-                window.location.href = '/404';
-            }       
-
-        }).catch(function (error) {
-            console.log(error);
-            alert("Oops, There was an unexpected error."); 
-        });
-
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert("Oops, There was an unexpected error."); 
-    });
-}
 
 function setPageVariableValue() {
     fin_num = addCommas(fin_num);   
