4f3c7a10fae6bc53bbeb6fb9684135f8e5fde1b4
michiamodavide <ashishavik11@icloud.com>
2021-09-08 15:18:46 +0530

    Update final-step.js

--- discoverfin/staging/final-step.js
+++ discoverfin/staging/final-step.js
@@ -13,66 +13,8 @@ $("#aptmt_link").attr("href", appointment_link);
 var rep_name = capitalize(readCookie("REP_NAME"));
 $(".rep_name").text(rep_name);
 $(".rep_name_cta").text(rep_name);
-
-$("#cta_btn").click(function () {
-  unique_id = readCookie("Unique ID");
-  user_id = readCookie("User ID");
-  name = readCookie("Name");
-  age = readCookie("Age");
-  retirement_age = readCookie("Retirement Age");
-  income_after_inflation = readCookie("Income After Inflation");
-  income_befor_inflation = readCookie("Income Before Inflation");
-  pension_choice = readCookie("Pension Choice");
-  guessed_fin = readCookie("Guessed FIN");
-  email = readCookie("Email");
-  fin_number = readCookie("FIN Number");
-  country_name = readCookie("Country");
-  route_selection = readCookie("Route Selection");
-  ques_1 = readCookie("question_1");
-  ques_2 = readCookie("question_2");
-  ques_3 = readCookie("question_3");
-  ques_4 = readCookie("question_4");
-  ques_5 = readCookie("question_5");
-
-  atomic("https://" + api_url + "/api/v1/users/submit/form", {
-    method: "POST",
-    headers: {
-        "Content-type": "application/json",
-    },
-    data: {
-      "Unique-Id": unique_id,
-      "User-Id": user_id,
-      "Name": name,
-      "Age": age,
-      "Retirement-Age": retirement_age,
-      "Annual-Income-after-inflation": income_after_inflation,
-      "Annual-Income-before-Inflation": income_befor_inflation,
-      "Pension-Choice": pension_choice,
-      "Guessed-FIN": guessed_fin,
-      "Email": email,
-      "Fin-Number": fin_number,
-      "country": country_name,
-      "route_choice": route_selection,
-      "ques_1": ques_1,
-      "ques_2": ques_2,
-      "ques_3": ques_3,
-      "hasRequestedVideoEmail": true,
-    },
-  })
-    .then(function (response) {
-      console.log(response.data); // xhr.responseText
-      console.log(response.xhr); // full response
-    })
-    .catch(function (error) {
-      console.log(error.status); // xhr.status
-      console.log(error.statusText); // xhr.statusText
-    });
-});
-
 $(".rep-phoito").css("background-image", "url('" + readCookie("PIC") + "')");
-
-
-$('.appointment-iframe .w-iframe iframe').attr('src','https://stagingdesktop.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
+$('.appointment-iframe .w-iframe iframe').attr('src','https://staging.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
 
 // learn more
 $('.slide_cta').click(function(){
@@ -84,12 +26,12 @@ $('.fin_learn_more').click(function(){
 });
 
 $('.iframe-back').click(function(){
-  $('.appointment-iframe .w-iframe iframe').attr('src','https://stagingdesktop.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
+  $('.appointment-iframe .w-iframe iframe').attr('src','https://staging.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
 });
 
 $('.closer-last').click(function(){
   $('.last-popup').removeClass('active');
-  $('.appointment-iframe .w-iframe iframe').attr('src','https://stagingdesktop.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
+  $('.appointment-iframe .w-iframe iframe').attr('src','https://staging.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
 });
 
 
