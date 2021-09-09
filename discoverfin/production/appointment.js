b0b37fd64866fcaf00ea4dfc4e5b8817b538f7f0
michiamodavide <ashishavik11@icloud.com>
2021-09-09 19:12:53 +0530

    Update appointment.js

--- discoverfin/production/appointment.js
+++ discoverfin/production/appointment.js
@@ -1,19 +1,3 @@
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-}
-
-
 var user_name = readCookie('Name');
 var appointment_link;
 var rep_name;
@@ -21,24 +5,28 @@ var rep_phone;
 var rep_email;
 var rep_pic;
 var video_id;
+var company_id;
+var user_id;
 var user = getUrlParameter('user');
 var company = getUrlParameter('company');
 
 atomic('https://' + api_url + '/api/v1/users/getCompany/name/' + company + '/' + user, {
     method: 'GET',
-
 })
 .then(function (response) {
     if (response.data.error == true) {
         console.log('Error');            
+        window.location.href = '/404';
     }
     else
     {       
         console.log(response.data);          
         appointment_link = response.data.data.appointmentBookingLink;
-        rep_name = response.data.data.firstName;
+        rep_name = response.data.data.firstName + " " + response.data.data.lastName;
         rep_pic = response.data.data.profilePic;
         rep_phone = response.data.data.phone;
+        user_id = response.data.data.userId;
+        company_id = response.data.data.companyId;
         rep_email = response.data.data.email;
         video_id = $.trim(response.data.data.videoProfileLink);
         map_all_data();
@@ -73,10 +61,7 @@ function map_all_data(){
 
     if(video_id){
         $('.apt-hero-bottomsection').addClass('active');
-        $('.apt-reps-video').append(video_id);
-        /*var video_container_width = (($('.apt-reps-video').width())/16)*9;
-        $('.apt-reps-video iframe').attr('height','100%');
-        $('.apt-reps-video iframe').attr('width','100%');        */
+        $('.apt-reps-video').append(video_id);                
     }    
 }
        
@@ -84,6 +69,30 @@ function map_all_data(){
 $('.closer-last').click(function(){
     $('#window_frame').attr("src","/appointment");
 });
+
+
+$('#getintouchsubmit').click(function(){
+    axios({
+        method: 'put',
+        url: 'https://' + api_url + '/api/v1/users/email/send/getInTouch',    
+        data: {
+            prospectFirstName: $('#first_name').val(),
+            prospectLastName: $('#last_name').val(),
+            prospectEmail: $('#email').val(),
+            prospectPhone: $('#phone_no').val(),
+            prospectMessage: $('#message').val(),
+            userId: company_id,
+            companyId: company_id
+        }   
+      }).then(function(response) {     
+        alert('Your Request has been submitted sucessfully!');
+      })
+      .catch(function (error) {
+          console.log(error);
+          alert("Oops, There was an unexpected error."); 
+      });
+});
+
 
 
 
