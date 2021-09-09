b977d298ce814832ce98d869ae6be1866946004e
michiamodavide <ashishavik11@icloud.com>
2021-09-09 19:13:21 +0530

    Update route.js

--- discoverfin/production/route.js
+++ discoverfin/production/route.js
@@ -1,72 +1,50 @@
+var company_id, prospect_id, route_selection, user_name, fin_num;
 
-// Reading name cookie
-var user_name = readCookie('Name');
-var fin_num = readCookie('FIN Number');
-fin_num = addCommas(fin_num);
-
-$('#user_name').html('' + user_name);
-$('#fin_num').html('$' + fin_num);
-
-function submitRoute(redirectUrl) {
-    unique_id = readCookie('Unique ID');
-    user_id = readCookie('User ID');
-    name = readCookie('Name');
-    age = readCookie('Age');
-    retirement_age = readCookie('Retirement Age');
-    income_after_inflation = readCookie('Income After Inflation');
-    income_befor_inflation = readCookie('Income Before Inflation');
-    pension_choice = readCookie('Pension Choice');
-    guessed_fin = readCookie('Guessed FIN');
-    email = readCookie('Email');
-    fin_number = readCookie('FIN Number');
-    country_name = readCookie('Country');
-    route_selection = readCookie('Route Selection');
-
-    atomic('https://' + api_url + '/api/v1/users/submit/form', {
-        method: 'POST',
-        data: {
-            'Unique-Id': unique_id,
-            'User-Id': user_id,
-            'Name': name,
-            'Age': age,
-            'Retirement-Age': retirement_age,
-            'Annual-Income-after-inflation': income_after_inflation,
-            'Annual-Income-before-Inflation': income_befor_inflation,
-            'Pension-Choice': pension_choice,
-            'Guessed-FIN': guessed_fin,
-            'Email': email,
-            'Fin-Number': fin_number,
-            'country': country_name,
-            'route_choice': route_selection,
-        },
-    })
-        .then(function (response) {
-            window.location.href = redirectUrl;
-            console.log(response.data); // xhr.responseText
-            console.log(response.xhr);  // full response
-        })
-        .catch(function (error) {
-            alert('Oops, something went wrong!');
-            console.log(error.status); // xhr.status
-            console.log(error.statusText); // xhr.statusText
-        });
+if(readCookie('COMPANY_ID') && readCookie('prospect_id') && readCookie('Name') && readCookie('FIN Number')){
+    company_id = readCookie('COMPANY_ID');     
+    prospect_id = readCookie('prospect_id');
+    user_name = readCookie('Name');
+    fin_num = readCookie('FIN Number');
+    setPageVariableValue();
+} else {
+    window.location.href = '/404';
 }
 
-function setCookies(field1, value1) {
-    document.cookie = field1 + "=" + value1 + ";path=/";
+
+function setPageVariableValue() {
+    fin_num = addCommas(fin_num);   
+    $('#user_name').html('' + user_name);
+    $('#fin_num').html('$' + fin_num);
 }
+
 
 $('#route_1').click(function () {
-    setCookies('Route Selection', 'Manage Money Better');
+    route_selection = 'Manage Money Better';
     submitRoute('/route/manage-money-better');
 });
 
 $('#route_2').click(function () {
-    setCookies('Route Selection', 'Make More Money');
+    route_selection = 'Make More Money';
     submitRoute('/route/make-more-money');
 });
 
 $('#route_3').click(function () {
-    setCookies('Route Selection', 'Both');
+    route_selection = 'Both';
     submitRoute('/route/both');
 });
+
+function submitRoute(redirectUrl) {
+    axios({
+        method: 'put',
+        url: 'https://' + api_url + '/api/v1/users/company/'+ company_id +'/prospects/' + prospect_id,    
+        data: {
+            route_choice: route_selection, 
+        }   
+    }).then(function(response) {     
+        window.location.href = redirectUrl;        
+    })
+    .catch(function (error) {
+        console.log(error);
+        alert("Oops, There was an unexpected error."); 
+    });  
+}
