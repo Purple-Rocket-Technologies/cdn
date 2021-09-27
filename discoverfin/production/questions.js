fa8a4d582cb9e6ca390113f90b81193d5b753215
michiamodavide <ashishavik11@icloud.com>
2021-09-28 00:29:31 +0530

    Update questions.js

--- discoverfin/production/questions.js
+++ discoverfin/production/questions.js
@@ -314,7 +314,7 @@ function updateProspect(prospectID){
     window.location.href = "/result";
   })
   .catch(function (error) {
-    alert("Oops, There was an unexpected error."); 
+    alert(error.response.data.message); 
   });  
 }
 
