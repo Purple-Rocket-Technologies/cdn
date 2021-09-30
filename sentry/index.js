75b47d9003373e39af8423b29326ecb931b0a41a
Harsh Vardhan Goswami <46974223+iamharshdev@users.noreply.github.com>
2021-09-30 23:21:12 +0530

    Update index.js

--- sentry/index.js
+++ sentry/index.js
@@ -1,5 +1,24 @@
-const catchExceptionToSentry = (type, data) => {
-  type === "string"
-    ? Sentry.captureMessage(data)
-    : Sentry.captureException(data);
-};
+class SentryError extends Error {
+  constructor(errMessage, data, type = "error") {
+    // Passes errMessage to the Error super class,
+    // similar to call new Error(errMessage).
+    super(errMessage);
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, SentryError);
+    }
+
+    this.name = "SentryError";
+
+    Sentry.addBreadcrumb({
+      category: "data",
+      message: errMessage,
+      data: data,
+      type: type,
+      level: Sentry.Severity.Debug,
+    });
+
+    Sentry.captureException(errMessage);
+  }
+}
