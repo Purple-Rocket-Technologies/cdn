ceb2c23d657c79eb3a931c0fccd8c8f0e1f937bf
Harsh Vardhan Goswami <46974223+iamharshdev@users.noreply.github.com>
2021-10-13 23:39:58 +0530

    Update final-step.js

--- discoverfin/development/final-step.js
+++ discoverfin/development/final-step.js
@@ -1,11 +1,16 @@
 //var height = ($(".video_placeholder").width() / 16) * 9;
 //$(".video_placeholder").height(height);
 
+var URL_COMPANY = readCookie('URL_COMPANY');
+var URL_USER = readCookie('URL_USER');
+
 // Reading name cookie
 var user_name = readCookie("Name");
 $(".user_name").each(function () {
   $(this).html("" + user_name);
 });
+
+console.log(URL_COMPANY, URL_USER);
 
 var appointment_link = "https://" + readCookie("APTMT_LINK");
 $("#aptmt_link").attr("href", appointment_link);
@@ -14,7 +19,7 @@ var rep_name = capitalize(readCookie("REP_NAME"));
 $(".rep_name").text(rep_name);
 $(".rep_name_cta").text(rep_name);
 $(".rep-phoito").css("background-image", "url('" + readCookie("PIC") + "')");
-$('.appointment-iframe .w-iframe iframe').attr('src','https://dev.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
+$('.appointment-iframe .w-iframe iframe').attr('src','https://dev.discoverfin.io/appointment?company=' + URL_COMPANY + '&user=' + URL_USER); 
 
 // learn more
 $('.slide_cta').click(function(){
@@ -26,12 +31,12 @@ $('.fin_learn_more').click(function(){
 });
 
 $('.iframe-back').click(function(){
-  $('.appointment-iframe .w-iframe iframe').attr('src','https://dev.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
+  $('.appointment-iframe .w-iframe iframe').attr('src','https://dev.discoverfin.io/appointment?company=' + URL_COMPANY + '&user=' + URL_USER); 
 });
 
 $('.closer-last').click(function(){
   $('.last-popup').removeClass('active');
-  $('.appointment-iframe .w-iframe iframe').attr('src','https://dev.discoverfin.io/appointment?company=' + readCookie('URL_COMPANY') + '&user=' + readCookie('URL_USER')); 
+  $('.appointment-iframe .w-iframe iframe').attr('src','https://dev.discoverfin.io/appointment?company=' + URL_COMPANY + '&user=' + URL_USER); 
 });
 
 
