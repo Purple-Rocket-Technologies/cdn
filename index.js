99b3919cad8c51e125e5870e2224f373db0c5bba
iamharshdev <nitingoswami473@gmail.com>
2021-11-02 19:39:42 +0530

    Added schedule btn on fin app.

---
+++ index.js
@@ -0,0 +1,18 @@
+if (
+  !window.location.pathname.startsWith("/questions") &&
+  window.location.hostname === "dev.discoverfin.io"
+) {
+  const URL_COMPANY = readCookie("URL_COMPANY");
+  const URL_USER = readCookie("URL_USER");
+
+  if ($("#schedule_btn").length > 0) {
+    $("#schedule_btn").css("display", "flex");
+    $("#schedule_btn").click(() => {
+      $(".last-popup").css("display", "flex");
+      $(".appointment-iframe .w-iframe iframe").attr(
+        "src",
+        `https://${window.location.hostname}/appointment-copy?company=${URL_COMPANY}&user=${URL_USER}&video=false`
+      );
+    });
+  }
+}
