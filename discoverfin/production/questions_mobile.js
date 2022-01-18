1e746111111192c2ac5c2f859b54905822379aae
iamharshdev <nitingoswami473@gmail.com>
2022-01-18 14:20:25 +0530

    Fix getresults flowq fin link

--- discoverfin/production/questions_mobile.js
+++ discoverfin/production/questions_mobile.js
@@ -387,30 +387,34 @@ async function updateProspect(prospectID) {
 }
 $("#submit_btn").click(function () {
   if ($("#terms").is(":checked")) {
-    axios({
-      method: "get",
-      url:
-        "https://" +
-        api_url +
-        "/api/v1/users/company/" +
-        readCookie("COMPANY_ID") +
-        "/prospects?email=" +
-        $("#email").val(),
-    })
-      .then(function (response) {
-        if (response.data.count === 0) {
-          createNewProspect();
-        } else {
-          updateProspect(response.data.data[0]._id);
-        }
+    if (!isEmail($("#email").val())) {
+      alert("Please enter your email address");
+    } else {
+      axios({
+        method: "get",
+        url:
+          "https://" +
+          api_url +
+          "/api/v1/users/company/" +
+          readCookie("COMPANY_ID") +
+          "/prospects?email=" +
+          $("#email").val(),
       })
-      .catch(function (error) {
-        alert("Oops, There was an unexpected error.");
-        throw new SentryError(
-          `Error While submitting answers: ${$("#email").val()}`,
-          error
-        );
-      });
+        .then(function (response) {
+          if (response.data.count === 0) {
+            createNewProspect();
+          } else {
+            updateProspect(response.data.data[0]._id);
+          }
+        })
+        .catch(function (error) {
+          alert("Oops, There was an unexpected error.");
+          throw new SentryError(
+            `Error While submitting answers: ${$("#email").val()}`,
+            error
+          );
+        });
+    }
   } else {
     alert("Please accept the terms and conditions");
   }
