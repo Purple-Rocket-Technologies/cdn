2a3731c767b9c80aa3067168246a93c3e1de75ed
Mayuresh Rele <68050307+MayureshRele@users.noreply.github.com>
2022-05-13 23:06:40 +0530

    video services added

--- src/service/video/index.js
+++ src/service/video/index.js
@@ -20,6 +20,27 @@ const getVideoProspect = async (COMPANY_ID, EMAIL) => {
   });
 };
 
+const createVideoProspect = async (COMPANY_ID, BODY) => {
+  const createNewVideoProspect = new Service(
+    `company/${COMPANY_ID}/videoProspects`
+  );
+  createNewVideoProspect.set(BODY);
+  return new Promise((resolve, reject) => {
+    createNewVideoProspect
+      .create()
+      .then((res) => {
+        if (res.count > 0) {
+          resolve(service.parseResponse(res.data));
+        } else {
+          reject(res);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
 async function fetchVideoService(type, country, lang) {
   const fetchVideoAPI = new Service("videoProspects/leadCapturingVideos");
   fetchVideoAPI.equals("type", type);
@@ -41,4 +62,134 @@ async function fetchVideoService(type, country, lang) {
   });
 }
 
-export { fetchVideoService, getVideoProspect };
+async function fetchValidateVideoType(type) {
+  const validateVideo = new Service("videoProspects/leadCapturingVideos");
+  validateVideo.equals("type", type);
+  return new Promise((resolve, reject) => {
+    validateVideo
+      .find()
+      .then((res) => {
+        if (res.count > 0) {
+          resolve(validateVideo);
+        } else {
+          reject(res);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+async function getPathsContentAPI(videoType) {
+  const patchContentAPI_URL = new Service("videoProspects/paths");
+  patchContentAPI_URL.equals("type", videoType);
+  return new Promise((resolve, reject) => {
+    patchContentAPI_URL
+      .find()
+      .then((res) => {
+        if (res.count > 0) {
+          resolve(patchContentAPI_URL);
+        } else {
+          reject(res);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+async function updateWatchTimeAPI(COMPANY_ID, PROSPECT_ID, UPDATE_DATA) {
+  return new Promise((resolve, reject) => {
+    const watchTime = new Service(
+      `company/${COMPANY_ID}/videoProspects/${PROSPECT_ID}`
+    );
+    watchTime.set(UPDATE_DATA);
+    watchTime
+      .update(PROSPECT_ID)
+      .then(function (response) {
+        if (response && response.data && response.data.data) {
+          resolve(prospectService.parseResponse(response.data));
+        } else {
+          reject(response);
+        }
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
+
+const updateVideoProspect = async (COMPANY_ID, PROSPECT_ID, BODY) => {
+  const updateVideoProspect = new Service(
+    `company/${COMPANY_ID}/videoProspects/${PROSPECT_ID}`
+  );
+  updateVideoProspect.set(BODY);
+  return new Promise((resolve, reject) => {
+    updateVideoProspect
+      .update()
+      .then((res) => {
+        if (res.count > 0) {
+          resolve(updateVideoProspect.parseResponse(res.data));
+        } else {
+          reject(res);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const getPathOptions = async (path_name) => {
+  const service = new Service(`videoProspects/paths?name=${path_name}`);
+  return new Promise((resolve, reject) => {
+    service
+      .find()
+      .then((res) => {
+        if (res.count > 0) {
+          resolve(service.parseResponse(res.data));
+        } else {
+          reject(res);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const setPathOptionsAPI = async (
+  COMPANY_ID,
+  VIDEO_PROSPECT_ID,
+  UPDATE_DATA
+) => {
+  return new Promise((resolve, reject) => {
+    const setpathAPI = new Service(`company/${COMPANY_ID}/videoProspects`);
+    setpathAPI.set(UPDATE_DATA);
+    setpathAPI
+      .update(VIDEO_PROSPECT_ID)
+      .then(function (response) {
+        if (response && response.data && response.data.data) {
+          resolve(prospectService.parseResponse(response.data));
+        } else {
+          reject(response);
+        }
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+};
+
+export {
+  fetchVideoService,
+  getVideoProspect,
+  fetchValidateVideoType,
+  getPathsContentAPI,
+  createVideoProspect,
+  updateWatchTimeAPI,
+  updateVideoProspect,
+  getPathOptions,
+  setPathOptionsAPI,
+};
