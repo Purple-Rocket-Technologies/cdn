6f179211a5c863a22050afa33cde3b98bca5fba5
MayureshRele <mayureshrele@gmail.com>
2023-01-26 18:12:21 +0530

    touch event updated

--- src/pages/apps/videomessages/index.js
+++ src/pages/apps/videomessages/index.js
@@ -63,8 +63,8 @@ export function init(advisorName = "") {
               },
               onmouseenter: () => (this.showOverLay = true),
               onmouseleave: () => (this.showOverLay = false),
-              ontouchstart:() => (this.showOverLay = true),
-              ontouchend:() => (this.showOverLay = false),
+              // ontouchstart:() => (this.showOverLay = true),
+              ontouchend:() => (!this.showOverLay),
             },
             [
               this.showOverLay
